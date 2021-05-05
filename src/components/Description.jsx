d53d77fd6b37e951481b47f4a88ab9f1dfc5b995
thaibang <thaibang1983hn@gmail.com>
2021-05-05 14:58:08 +0200

    rewrite the description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -6,21 +6,18 @@ function Description() {
       <div className="Description">
         <h1>Description</h1>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Repudiandaeab numquam eius tempore temporibus corporis, laborum
-          similiquedignissimos mollitia soluta laboriosam necessitatibus, eum
-          doloresquasi tenetur quo officia a amet.Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Repudiandae ab numquam eius tempore
-          temporibus corporis, laborum similique dignissimos mollitia soluta
-          laboriosam necessitatibus, eum dolores quasi tenetur quo officia a
-          amet.Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Repudiandae ab numquam eius tempore temporibus corporis, laborum
-          similique dignissimos mollitia soluta laboriosam necessitatibus, eum
-          dolores quasi tenetur quo officia a amet.Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Repudiandae ab numquam eius tempore
-          temporibus corporis, laborum similique dignissimos mollitia soluta
-          laboriosam necessitatibus, eum dolores quasi tenetur quo officia a
-          amet.
+          APIs are methods and protocols to connect with other libraries and
+          applications. It stands for Application Programming Interface.The API
+          provides the ability to provide access to a set of commonly used
+          functions that are possible to exchange data between applications.
+          Most websites apply to the Web API to connect, retrieve data, or
+          update the database. To provide the user a friendly environment to
+          find information of the other developer on the Website of Github,
+          which is very popular for all Web developers, we have made this
+          website where users can find all the things that they want to know
+          about one developer like the lastest repositories, the starred
+          repositories or anything they want to know. Hopefully, users will have
+          a pleasant experience when they visit our website.
         </p>
       </div>
     </StyledDesription>
