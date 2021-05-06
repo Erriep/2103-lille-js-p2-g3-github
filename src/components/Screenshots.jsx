4e8d8f257d51be043176730ea37ced96d1396789
Hacene010 <hamrounehacene06@gmail.com>
2021-05-06 14:24:14 +0200

    changed the wh to px, changed the pics of the screenshots

--- src/components/Screenshots.jsx
+++ src/components/Screenshots.jsx
@@ -5,16 +5,8 @@ function Screenshots() {
     <StyledScreenShot>
       <h1 className="titleAPI">EXAMPLES OF API</h1>
       <article className="Events">
-        <img
-          className="event1"
-          src="https://cdn.discordapp.com/attachments/828909702665601046/837261572186308618/unknown.png"
-          alt="ScreenShots"
-        />
-        <img
-          className="event2"
-          src="https://cdn.discordapp.com/attachments/828909702665601046/837261782405218314/unknown.png"
-          alt="ScreenShots"
-        />
+        <img className="event1" src="./img/screenshot1.png" alt="ScreenShots" />
+        <img className="event2" src="./img/screenshot2.png" alt="ScreenShots" />
       </article>
     </StyledScreenShot>
   );
